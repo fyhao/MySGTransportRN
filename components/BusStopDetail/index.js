21d48c783952c3f4a18ac0bac1e853fc67a0fc32
fyhao <fyhao1@gmail.com>
2019-07-27 20:00:39 +0800

    show map

--- components/BusStopDetail/index.js
+++ components/BusStopDetail/index.js
@@ -1,9 +1,9 @@
 import React, { PureComponent } from 'react';
 //import UI from react-native
-import { View, Text, Image, FlatList } from 'react-native';
+import { View, ScrollView, Text, Image, FlatList } from 'react-native';
 //import styles for component.
 import styles from './styles';
-
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 class BusStopDetail extends PureComponent {
     //Define your navigationOptions as a functino to have access to navigation properties, since it is static.
     static navigationOptions = ({navigation}) => ({
@@ -16,13 +16,31 @@ class BusStopDetail extends PureComponent {
 		const data = require('../../assets/data/stopServiceData.json');
 		var busStopCode = navigation.getParam('item').BusStopCode;
 		var services = data[busStopCode];
-        
+        var region={
+		  latitude: navigation.getParam('item').Latitude,
+		  longitude: navigation.getParam('item').Longitude,
+		  latitudeDelta: 0.0922,
+		  longitudeDelta: 0.0421,
+		};
         return (
-            <View style={styles.container}>
+            <ScrollView style={{flex: 1}}>
                 <Image source={{uri: 'https://res.cloudinary.com/aa1997/image/upload/v1535930682/pokeball-image.jpg'}}
                         style={styles.pokemonImage} />
                 <Text>{navigation.getParam('item').Description}</Text>
-				<FlatList 
+				
+				<View style={{height: 400}}>
+					<MapView 
+						initialRegion={region}
+						showsUserLocation={true}
+						followsUserLocation={true}
+						showsTraffic={true}
+						style={{flex: 1}}
+						>
+						<Marker coordinate={region}/>
+					</MapView>
+				  </View>
+				  
+				 <FlatList 
                     data={services}
                     renderItem={(data) => <View>
 						<Text>Service No: {data.item.ServiceNo}</Text>
@@ -31,7 +49,7 @@ class BusStopDetail extends PureComponent {
 					</View>}
                     keyExtractor={(item) => item.ServiceNo} 
                     />
-            </View>
+            </ScrollView>
         );
     }
 }
